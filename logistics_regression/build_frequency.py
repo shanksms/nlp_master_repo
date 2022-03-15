4271775dd4da7524031aa707a2135e293baebc8d
shanksms <sriv.srivastava@gmail.com>
2022-03-15 11:58:14 +0530

    build frequencies and plot graph

--- logistics_regression/build_frequency.py
+++ logistics_regression/build_frequency.py
@@ -83,4 +83,50 @@ if __name__ == '__main__':
 
     # check length of the dictionary
     print(f'len(freqs) = {len(freqs)}')
+    keys = ['happi', 'merri', 'nice', 'good', 'bad', 'sad', 'mad', 'best', 'pretti',
+            '❤', ':)', ':(', '😒', '😬', '😄', '😍', '♛',
+            'song', 'idea', 'power', 'play', 'magnific']
+    # list representing our table of word counts.
+    # each element consist of a sublist with this pattern: [<word>, <positive_count>, <negative_count>]
+    data = []
+
+    # loop through our selected words
+    for word in keys:
+
+        # initialize positive and negative counts
+        pos = 0
+        neg = 0
+
+        # retrieve number of positive counts
+        if (word, 1) in freqs:
+            pos = freqs[(word, 1)]
+
+        # retrieve number of negative counts
+        if (word, 0) in freqs:
+            neg = freqs[(word, 0)]
+
+        # append the word counts to the table
+        data.append([word, pos, neg])
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    # convert positive raw counts to logarithmic scale. we add 1 to avoid log(0)
+    x = np.log([x[1] + 1 for x in data])
+
+    # do the same for the negative counts
+    y = np.log([x[2] + 1 for x in data])
+
+    # Plot a dot for each pair of words
+    ax.scatter(x, y)
+
+    # assign axis labels
+    plt.xlabel("Log Positive count")
+    plt.ylabel("Log Negative count")
+
+    # Add the word as the label at the same position as you added the points just before
+    for i in range(0, len(data)):
+        ax.annotate(data[i][0], (x[i], y[i]), fontsize=12)
+
+    ax.plot([0, 9], [0, 9], color='red')  # Plot the red line that divides the 2 areas.
+    plt.show()
 
